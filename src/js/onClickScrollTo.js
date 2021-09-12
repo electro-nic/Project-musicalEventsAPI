4f36427d8da8c349a01814b49bda57ece1d120d1
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-12 15:35:07 +0300

    Добавив розмітку стилі та скрипт для кнопки скролу вверх

---
+++ src/js/onClickScrollTo.js
@@ -0,0 +1,24 @@
+import { refs } from './refs';
+
+window.addEventListener('scroll', onVisibleBtn);
+refs.btnToTop.addEventListener('click', onClickScrollTop);
+
+
+function onVisibleBtn () {
+    const scroll = window.pageYOffset;
+    const height = document.documentElement.clientHeight;
+    if(scroll > height) {
+        refs.btnToTop.classList.remove('is-hidden');
+    } if (scroll < height) {
+        refs.btnToTop.classList.add('is-hidden');
+    }
+    };
+
+  function onClickScrollTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+
+ }
---
+++ src/js/refs.js
@@ -0,0 +1,3 @@
+export const refs = {
+    btnToTop: document.querySelector('.js-button'),
+}
