08c39b36a9f77fd0a67a42b9268a1f0b4de33adc
Nikolayhous <55892018+Nikolayhous@users.noreply.github.com>
2021-09-12 11:39:30 +0000

    Deploying to gh-pages from @ Nikolayhous/Project-musicalEventsAPI@7d9bd1d158b7e3781c654e2cb65d7e6b5e2929a7 🚀

--- src.b81ac760.js
+++ src.b81ac760.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Project-musicalEventsAPI/src.3ba3c865.js.map
+//# sourceMappingURL=/Project-musicalEventsAPI/src.b81ac760.js.map
