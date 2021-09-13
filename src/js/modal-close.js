5a9ddee7c711e7c32b5d4241632890189df79947
Ganna <annakovalenko168@gmail.com>
2021-09-13 12:36:44 +0300

    наброски по закрытию модалки

--- src/js/modal-close.js
+++ src/js/modal-close.js
@@ -2,13 +2,25 @@
     const refs = {
     //   openModalLink: document.querySelector('.'),
       closeModalBtn: document.querySelector('.modal__close'),
+      closeModalWindow: document.querySelector('.js-modal'),
       modal: document.querySelector('.modal__backdrop'),
+
     };
   
     // refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalWindow.addEventListener('keydown', keyEscape);
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+      // window.removeEventListener('keydown', keyEscape);
     }
+
+  //   function closeModalWindow() {
+  //     refs.createModalImg.classList.remove('is-open');
+  //     refs.poster.src ="#";
+  //     refs.poster.alt ="#";
+  
+  //     // window.removeEventListener('keydown', keyEscape);
+  //  }
   })();
