71dee9773271ee9b4523f33ac56e9f2fcaadecbf
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-13 22:35:48 +0300

    Прописал скрипт для рендера галереи

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export const refs = {
     form: document.querySelector('.form'),
-    eventList: document.querySelector('.js-events__list'),
+    eventList: document.querySelector('#js-events__list'),
     btnToTop: document.querySelector('.js-button'),
     inputCountry:document.querySelector('#country'),
     countriesList:document.querySelector('.all-countries-list')
