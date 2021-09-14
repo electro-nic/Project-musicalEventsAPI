56baca1b3c6fbe79bad31f37f34f3b3dba89f184
Ganna <annakovalenko168@gmail.com>
2021-09-14 15:15:39 +0300

    модалка

--- src/js/modal-close.js
+++ src/js/modal-close.js
@@ -1,14 +1,51 @@
-(() => {
-    const refs = {
-    //   openModalLink: document.querySelector('.'),
-      closeModalBtn: document.querySelector('.modal__close'),
-      modal: document.querySelector('.modal__backdrop'),
+// (() => {
+//     const refs = {
+//       openModalLink: document.querySelector('[data-modal-open]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       modal: document.querySelector('[data-modal]'),
+//     };
+  
+//     refs.openModalLink.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+//     function toggleModal() {
+//       refs.modal.classList.toggle('is-hidden');
+//     }
+//   })();
+
+import { refs } from "./refs";
+
+const closeModal = document.querySelector('.modal__backdrop');
+const closeModalBtn = document.querySelector('.modal__close');
+const backdrop = document.querySelector('.modal__backdrop');
+
+refs.eventList.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeWindow);
+closeModal.addEventListener('click', closeWindow);
+
+
+function openModal(e) {
+  e.preventDefault();
+  if(e.target.nodeName === 'LI') {
+    document.closeModal.classList.remove('is-hidden');
+  }
+  // else {
+  //   return;
+  // }
+  // window.addEventListener('keydown', keyEscape);
+
+  console.log(e.target.nodeName);
+};
+
+function closeWindow(){
+   document.closeModal.classList.add('is-hidden');
+   
+      // console.log(e.target.nodeName);
     };
-  
-    // refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+
+function keyEscape(e) {
+  if(e.code === "Escape") {
+    closeWindow();
+  }
+}
+
