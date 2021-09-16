d3dd5dceb2f8f6ef8351fab1700194c122a13322
marta <Vynnytska1989@gmail.com>
2021-09-16 21:19:15 +0300

    2 варіанти селекту

--- src/js/input-country.js
+++ src/js/input-country.js
@@ -1,43 +1,52 @@
 
-
 import {refs} from './refs';
-import cardCountry from '../templates/country'; 
 
 const API_KEY = 'Ci1vLtAQ5toUQm0alN6gL6AfnGn8TpWy'; //'vHwA8wPXBKX2TH3dmOzAbzhop6A7jZPG';
 const BASE_URL = 'https://app.ticketmaster.com/discovery-feed/v2/events?';
 
-async function fetchCountries(name) {
+async function fetchCountries(countryCode) {
   const promiseCoutries = await fetch(`${BASE_URL}apikey=${API_KEY}`);
   const countryData = promiseCoutries.json();
   return countryData;
   
 }
 
-refs.inputCountry.addEventListener('click',onCountryBtnClick)
+
+refs.inputCountry.addEventListener('click', onCountryBtnClick)
+// refs.countryBtn.addEventListener('click',onCountryBtnClick)
 
 async function onCountryBtnClick(e){
   e.preventDefault();
   try{
     const countryFetch = await fetchCountries(country)
     .then(elems=>
-       onCountryCreate(elems.countries))
-    
-  }
+      // console.log(elems)
+       onCountryCreate(elems.countries)
+    )}
   catch(err){
   console.log(err);
 }
 }
-function onCountryCreate (obj){
+async function onCountryCreate (obj){
     const optionEl= Object.keys(obj)
     console.log(optionEl)
-  const listCountry=optionEl.map((el)=>{
-    const itemCountries = `<option class='input-country' value="${el}">${el}</option>`;
-    // itemCountries.append(el)
-    console.log(itemCountries)
-    return itemCountries;
-  })
+    const listCountry = optionEl.map((el) => {
+      const itemCountries = `<option class='input-country' value="${el}">${el}</option>`;
+      console.log(itemCountries);
+      return itemCountries;
+    })
   refs.inputCountry.insertAdjacentHTML('beforeend',listCountry)
+}
+// function createCountriesSelect() {
+//   const options = countryCodes.map(({ alphaCode, name }) => {
+//     // деструктуризація елементів масиву обєктів країн
+//     return <option class='optin-coutries-list' value="${alphaCode}">${name}</option>; //
+//   });
+//   selectRef.insertAdjacentHTML('beforeend', options.join('')); 
 // }
+// createCountriesSelect();
+
+
 
   //   const listMarkup = cardCountry(optionEl);
 
@@ -47,7 +56,7 @@ function onCountryCreate (obj){
   //    return `<option  value="${el.country_code}">${el.country_code}</option>`})
   //     refs.menu.insertAdjacentHTML('beforeend',options.join(''))
 
-   }
+  //  }
   //   const listCountry = optionEl.map((el)=>{
   //   console.log(el);
   //   const itemCountries = document.createElement('option');
