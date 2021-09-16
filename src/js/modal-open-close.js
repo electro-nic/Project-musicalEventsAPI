b63e77c3e3f662b9248c10462eb61d53ca33d83f
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-09-16 15:20:11 +0300

    открытие модалки - багфикс

---
+++ src/js/modal-open-close.js
@@ -0,0 +1,50 @@
+import { refs } from './refs';
+import { renderModalInfo } from './renderModalInfo';
+
+const closeModalWindow = document.querySelector('.modal__backdrop');
+const closeModalBtn = document.querySelector(
+  '[data-action="close-modal__backdrop"]'
+);
+
+closeModalBtn.addEventListener('click', closeModal);
+closeModalWindow.addEventListener('click', closeModal);
+
+export function openModal(e) {
+  closeModalWindow.classList.remove('is-hidden');
+  console.log('e.currentTarget', e.currentTarget);
+  console.log('e.target', e.target);
+
+  const index = e.currentTarget.dataset.index;
+  window.addEventListener('keydown', keyEscape);
+
+  renderModalInfo(index);
+}
+
+function closeModal(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    e.preventDefault();
+    closeModalWindow.classList.add('is-hidden');
+
+    console.log(e.target.nodeName);
+  }
+  if (e.target.nodeName === 'FORM') {
+    e.preventDefault();
+    closeModalWindow.classList.add('is-hidden');
+
+    console.log(e.target.nodeName);
+  }
+  if (e.target.nodeName === 'SPAN') {
+    e.preventDefault();
+    closeModalWindow.classList.add('is-hidden');
+  } else {
+    return;
+  }
+
+  console.log(e.target.nodeName);
+}
+
+function keyEscape(e) {
+  if (e.code == 'Escape') {
+    closeModal();
+  }
+}
