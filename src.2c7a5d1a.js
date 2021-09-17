0fcd0d456aa6e9556922a13b69337194cef07c16
Nikolayhous <55892018+Nikolayhous@users.noreply.github.com>
2021-09-17 08:55:16 +0000

    Deploying to gh-pages from @ Nikolayhous/Project-musicalEventsAPI@84da5203140b60cda387b82837beeb15765ed150 🚀

--- src.2c7a5d1a.js
+++ src.2c7a5d1a.js
@@ -43,4 +43,4 @@ var t;!function(e,n){"object"==typeof exports&&"object"==typeof module?module.ex
 },{"tui-pagination":"jPMh","../services/api-services":"SXvC"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/renderGalleryCards"),require("./js/onClickScrollTo"),require("./js/modal-close"),require("./js/preloader"),require("./js/input-search"),require("./js/pagination"),require("./js/variables"),require("./js/renderModalInfo");
 },{"./sass/main.scss":"clu1","./js/renderGalleryCards":"cntR","./js/onClickScrollTo":"qg96","./js/modal-close":"fdXB","./js/preloader":"BeZ8","./js/input-search":"zCmT","./js/pagination":"o8lo","./js/variables":"u9EY","./js/renderModalInfo":"MRW2"}]},{},["Focm"], null)
-//# sourceMappingURL=/Project-musicalEventsAPI/src.b1d88814.js.map
+//# sourceMappingURL=/Project-musicalEventsAPI/src.2c7a5d1a.js.map
