76e76524e8403a201dbc942672e819d2f99821f9
IhorHrytsenko <gisyshka@gmail.com>
2021-09-18 23:12:41 +0300

    FIXAPI-Ihor
    Сделал, чтобы теперь функция возвращала объект, именно объект. Сделал его точно таким же как и результат вызова, чтобы у ребят не ломалась логика.
    
    Закомментировал секцию с циклом.

--- src/js/api-connect.js
+++ src/js/api-connect.js
@@ -1,37 +1,40 @@
 const API_KEY = 'AGcGkGLirWXVP1TBWnNfEanVNPgcYNS9';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
-let arr = [];
-let currentPage = 0;
-
 
 export default async function connect(keyword = '', page = 0, size = 20, countryCode = '') {
 
     try {
+        const objData = {
+            page : page,
+            _embedded: {events: []},
+            _links: [],
+        }
+        let currentPage = 1;
         const promiseResponse = await fetch(`${BASE_URL}classificationName=music&apikey=${API_KEY}&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}`);
         const data = await promiseResponse.json();
 
-        // return data;
-
-        if (data.page.totalPages > 1) {   
-        for (let i = 0; i < 4; i++) {
-        const promiseResponse = await fetch(`${BASE_URL}classificationName=music&dmaId=324&apikey=${API_KEY}&keyword=${keyword}&page=${currentPage}&size=${size}&countryCode=${countryCode}`);
-        const data = await promiseResponse.json();
-            arr.push(...data._embedded.events);
-            ++currentPage;
-            }
-            
-            // console.log(`${keyword} ` , arr);
-            currentPage = 0;
-
-            return data;
-    }
-
         
-        else {
-            return data;
-        }
+        objData.page = data.page;
+        objData._links = data._links;
+        objData._embedded.events.push(...data._embedded.events);  
 
+        return objData;
 
+    //     if (data.page.totalPages > 1) {   
+    //     for (let i = 1; i < 4; i++) {
+    //     const promiseResponse = await fetch(`${BASE_URL}classificationName=music&dmaId=324&apikey=${API_KEY}&keyword=${keyword}&page=${currentPage}&size=${size}&countryCode=${countryCode}`);
+    //     const data = await promiseResponse.json();
+    //         objData._embedded.events.push(...data._embedded.events);            
+    //         ++currentPage;
+    //         }
+
+    //         currentPage = 1;
+    //         return objData;
+    // }
+  
+    //     else {
+    //         return data;
+    //     }
 
     }
     catch (err) {
@@ -44,9 +47,7 @@ export default async function connect(keyword = '', page = 0, size = 20, country
 // Sorting order of the search result. Allowable values : 'name,asc', 'name,desc', 'date,asc', 'date,desc', 'relevance,asc', 'relevance,desc', 'distance,asc', 'name,date,asc', 'name,date,desc', 'date,name,asc', 'date,name,desc', 'distance,date,asc', 'onSaleStartDate,asc', 'id,asc', 'venueName,asc', 'venueName,desc', 'random'
 
 //вызывать функцию так
-const obj = connect('', 0, 20, '');
-
-obj.then(log => console.log(log));
+connect('', 0, 20, '').then(data => console.log(data));
 
 
 
