eb842679b9186a985d0a2eb52fb8142fb64ca48e
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-21 15:09:15 +0300

    Змінив сорт в пошуку АРІ

--- src/js/api-connect.js
+++ src/js/api-connect.js
@@ -11,7 +11,7 @@ export default async function connect(keyword = '', page = 0, size = 20, country
             _links: [],
         }
         let currentPage = 1;
-        const promiseResponse = await fetch(`${BASE_URL}classificationName=music&apikey=${API_KEY}&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=relevance,asc`);
+        const promiseResponse = await fetch(`${BASE_URL}classificationName=music&apikey=${API_KEY}&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=date,asc`);
         const data = await promiseResponse.json();
 
         objData.page = data.page;
