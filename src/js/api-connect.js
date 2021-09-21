6e3861565d190d7959517fa56316ecf93e8da9a0
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-21 23:05:24 +0300

    АРІ

--- src/js/api-connect.js
+++ src/js/api-connect.js
@@ -1,44 +1,14 @@
 
-const API_KEY = 'Ci1vLtAQ5toUQm0alN6gL6AfnGn8TpWy';
+const API_KEY = 'mi18OgMaUPrYWCki5ARgZtzyNs6tdZjH';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 
 export default async function connect(keyword = '', page = 0, size = 20, countryCode = '') {
 
     try {
-            const promiseResponse = await fetch(`${BASE_URL}classificationName=music&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=date,desc&apikey=${API_KEY}`);
+            const promiseResponse = await fetch(`${BASE_URL}classificationName=music&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=date,asc&apikey=${API_KEY}`);
             const data = await promiseResponse.json();
 
             return data;
-
-        const objData = {
-            page : page,
-            _embedded: {events: []},
-            _links: [],
-        }
-        let currentPage = 1;
-        const promiseResponse = await fetch(`${BASE_URL}classificationName=music&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=date,asc&apikey=${API_KEY}`);
-        const data = await promiseResponse.json();
-
-        objData.page = data.page;
-        objData._links = data._links;
-        objData._embedded.events.push(...data._embedded.events);  
-
-        return objData;
-
-    //     if (data.page.totalPages > 1) {   
-    //     for (let i = 1; i < 4; i++) {
-    //     const promiseResponse = await fetch(`${BASE_URL}classificationName=music&dmaId=324&apikey=${API_KEY}&keyword=${keyword}&page=${currentPage}&size=${size}&countryCode=${countryCode}`);
-    //     const data = await promiseResponse.json();
-    //         objData._embedded.events.push(...data._embedded.events);            
-    //         ++currentPage;
-    //         }
-    //         currentPage = 1;
-    //         return objData;
-    // }
-    //     else {
-    //         return data;
-    //     }
-
     }
     catch (err) {
         console.log(err);
@@ -51,7 +21,3 @@ export default async function connect(keyword = '', page = 0, size = 20, country
 
 //вызывать функцию так
 // connect('', 0, 20, '').then(data => console.log(data));
-
-
-
-
