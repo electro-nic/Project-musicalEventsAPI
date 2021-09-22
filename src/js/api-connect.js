21c64a636386b65b35fbbc68af102e8d6e40616b
YEVHEN BATSMAN <evgenbatsman@gmail.com>
2021-09-22 22:22:56 +0300

    шатаю api для 21 события
    Принимать только после ревью игоря или если нет ошибок

--- src/js/api-connect.js
+++ src/js/api-connect.js
@@ -2,9 +2,16 @@
 const API_KEY = 'mi18OgMaUPrYWCki5ARgZtzyNs6tdZjH';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 
-export default async function connect(keyword = '', page = 0, size = 20, countryCode = '') {
+export default async function connect(keyword = '', page = 0, size = '', countryCode = '') {
 
-    try {
+    try {   
+            const windowOuterWidth = window.outerWidth;
+
+            if (windowOuterWidth > 767 && windowOuterWidth < 1280) {
+                size = 21;
+            } else {
+                size = 20;
+            }
             const promiseResponse = await fetch(`${BASE_URL}classificationName=music&keyword=${keyword}&page=${page}&size=${size}&countryCode=${countryCode}&sort=venueName,desc&apikey=${API_KEY}`);
             const data = await promiseResponse.json();
 
